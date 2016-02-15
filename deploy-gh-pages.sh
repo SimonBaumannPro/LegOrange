be84d1fb7ac3b2f9318e19c9c20a1dee53e6b212
SimonBaumannPro <simonbaumann20@gmail.com>
2016-02-15 15:44:13 +0000

    add tags

---
+++ deploy-gh-pages.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+cd dist
+git init
+git config user.name "SimonBaumannPro"
+git config user.email "webcom.ops@orange.com"
+git add .
+git commit -m "Deploy to GitHub Pages"
+git push --force --quiet "https://${GH_TOKEN}@github.com/${TRAVIS_REPO_SLUG}.git" master:gh-pages > /dev/null 2>&1
