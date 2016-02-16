83c1dbb7bbd649bf1801d118a78dc242f70a9090
SimonBaumannPro <simonbaumann20@gmail.com>
2016-02-16 10:35:20 +0000

    email modified

--- deploy-gh-pages.sh
+++ deploy-gh-pages.sh
@@ -3,7 +3,7 @@
 cd dist
 git init
 git config user.name "SimonBaumannPro"
-git config user.email "simonbaumann20@gmail.com"
+git config user.email "simon.baumann@orange.com"
 git add .
 git commit -m "Deploy to GitHub Pages"
 git push --force --quiet "https://${GH_TOKEN}@github.com/${TRAVIS_REPO_SLUG}.git" master:gh-pages > /dev/null 2>&1
